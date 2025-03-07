073ca508c080c985ea7caaf04d9f84186ed29ddc
marcinbogdanowicz <marcin.bogdanowicz@wp.eu>
2025-03-07 17:59:01 +0100

    Adjust scripts/command.sh to run Django commands

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-docker compose run --rm -w /app refund_request_processing_system bash -c "$@"
+docker compose run --rm -w /app/refund_request_processing_system refund_request_processing_system python manage.py "$@"
 
