c1d59fd63a8d09364074df08250023c00cac08a1
marcinbogdanowicz <marcin.bogdanowicz@wp.eu>
2025-03-10 18:25:25 +0100

    In refund request creation form: add predefined choices for reason, show other reason field only if 'Other' is selected

--- refund_request_processing_system/apps/refunds/enums.py
+++ refund_request_processing_system/apps/refunds/enums.py
@@ -5,3 +5,14 @@ class RefundStatus(StrEnum):
     PENDING = 'pending'
     APPROVED = 'approved'
     REJECTED = 'rejected'
+
+
+class RefundReason(StrEnum):
+    WRONG_PRODUCT = 'Wrong product'
+    PRODUCT_DAMAGED = 'Product damaged'
+    PRODUCT_DEFECTIVE = 'Product defective'
+    OTHER = 'Other'
+
+    @classmethod
+    def as_choices(cls):
+        return [(choice.value, choice.value) for choice in cls]
--- refund_request_processing_system/apps/refunds/forms.py
+++ refund_request_processing_system/apps/refunds/forms.py
@@ -4,18 +4,25 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 from apps.core.mixins import BootstrapFormMixin
+from apps.refunds.enums import RefundReason
 from apps.refunds.models import RefundRequest
 from apps.refunds.utils import IBANValidator
 
 
 class RefundRequestForm(BootstrapFormMixin, forms.ModelForm):
+    reason_choice = forms.ChoiceField(
+        choices=RefundReason.as_choices(), required=True, label='Reason'
+    )
+    other_reason = forms.CharField(required=False)
+
     class Meta:
         model = RefundRequest
         fields = [
             'order_number',
             'order_date',
             'products',
-            'reason',
+            'reason_choice',
+            'other_reason',
             'first_name',
             'last_name',
             'phone_number',
@@ -31,7 +38,7 @@ class RefundRequestForm(BootstrapFormMixin, forms.ModelForm):
         widgets = {
             'address': forms.Textarea(attrs={'rows': 2}),
             'products': forms.Textarea(attrs={'rows': 3}),
-            'reason': forms.Textarea(attrs={'rows': 3}),
+            'other_reason': forms.Textarea(attrs={'rows': 3}),
             'iban': forms.TextInput(
                 attrs={'pattern': '[A-Z]{2}[0-9]{2}[0-9A-Z]{1,30}'}
             ),
@@ -39,6 +46,13 @@ class RefundRequestForm(BootstrapFormMixin, forms.ModelForm):
         help_texts = {
             'iban': 'Only digits and upper case letters. E.g. \'PL10105000997603123456789123\'',
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            if self.instance.reason not in RefundReason:
+                self.fields['reason_choice'].initial = RefundReason.OTHER
+                self.fields['other_reason'].initial = self.instance.reason
 
     @classmethod
     def initial_for_user(cls, user):
@@ -71,7 +85,12 @@ class RefundRequestForm(BootstrapFormMixin, forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
+        self._custom_clean_iban(cleaned_data)
+        self._custom_clean_reason(cleaned_data)
 
+        return cleaned_data
+
+    def _custom_clean_iban(self, cleaned_data):
         iban = cleaned_data.get('iban')
         country = cleaned_data.get('country')
 
@@ -82,4 +101,14 @@ class RefundRequestForm(BootstrapFormMixin, forms.ModelForm):
 
             self.instance.iban_verified = True
 
-        return cleaned_data
+    def _custom_clean_reason(self, cleaned_data):
+        reason_choice = cleaned_data.get('reason_choice')
+        other_reason = cleaned_data.get('other_reason')
+
+        if reason_choice == RefundReason.OTHER:
+            if not other_reason:
+                self.add_error('other_reason', 'This field is required.')
+            else:
+                self.instance.reason = other_reason
+        else:
+            self.instance.reason = reason_choice
